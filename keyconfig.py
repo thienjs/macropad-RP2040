4b0bb503553d1f8f02050cf9b950abfc3b7dd194
thienjs <thien.tran6@gmail.com>
2023-12-08 22:43:51 -0500

    first commit

---
+++ keyconfig.py
@@ -0,0 +1,11 @@
+# Constants for keys and colors in display and LEDs
+
+# Key labels
+KEY_BLANK = '[===]'
+KEY_MUTE = 'Mute'
+KEY_VOL_UP = 'Vol+'
+KEY_VOL_DOWN = 'Vol-'
+
+# LED colors
+LED_BLANK = 0x000000
+LED_PRESS = 0xFFFFFF
