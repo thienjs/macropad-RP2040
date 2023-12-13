7f8f59626846cbcaed1dfe047af91eab10fc5ead
thienjs <thien.tran6@gmail.com>
2023-12-13 08:37:45 -0500

    chrome macros

--- macros/numpad.py
+++ macros/numpad.py
@@ -1,27 +1,26 @@
-# MACROPAD Hotkeys: Universal Numpad
+# Universal Numpad
 
 from adafruit_hid.keycode import Keycode
 
 app = {
-    'name' : 'Numpad',
-    'order': 2,
+    'name' : 'Numpad', # Application name
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x004166, '7    ', [Keycode.KEYPAD_SEVEN]),
-        (0x004166, '8    ', [Keycode.KEYPAD_EIGHT]),
-        (0x004166, '9    ', [Keycode.KEYPAD_NINE]),
+        (0x004166, '7', ['7']),
+        (0x004166, '8', ['8']),
+        (0x004166, '9', ['9']),
         # 2nd row ----------
-        (0x004166, '4    ', [Keycode.KEYPAD_FOUR]),
-        (0x004166, '5    ', [Keycode.KEYPAD_FIVE]),
-        (0x004166, '6    ', [Keycode.KEYPAD_SIX]),
+        (0x004166, '4', ['4']),
+        (0x004166, '5', ['5']),
+        (0x004166, '6', ['6']),
         # 3rd row ----------
-        (0x004166, '1    ', [Keycode.KEYPAD_ONE]),
-        (0x004166, '2    ', [Keycode.KEYPAD_TWO]),
-        (0x004166, '3    ', [Keycode.KEYPAD_THREE]),
+        (0x004166, '1', ['1']),
+        (0x004166, '2', ['2']),
+        (0x004166, '3', ['3']),
         # 4th row ----------
-        (0x004166, '0    ', [Keycode.KEYPAD_ZERO]),
-        (0x640A66, '.    ', [Keycode.KEYPAD_PERIOD]),
-        (0x663F0A, 'ENTER', [Keycode.KEYPAD_ENTER])
+        (0x004166, '0', ['0']),
+        (0x640A66, '.', ['.']),
+        (0x002000, 'ENTER', [Keycode.ENTER]),
     ]
 }
--- macros/web.py
+++ macros/web.py
@@ -7,29 +7,26 @@
 from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
 
 app = {                      # REQUIRED dict, must be named 'app'
-    'name': 'Web Browsing',  # Application name
+    'name': 'Chrome',  # Application name
     'macros': [             # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x004000, '< Back', [Keycode.ALT, Keycode.LEFT_ARROW]),
-        (0x004000, 'Fwd >', [Keycode.ALT, Keycode.RIGHT_ARROW]),
-        (0x400000, 'Up', [Keycode.SHIFT, ' ']),      # Scroll up
+        (0x004000, '< Back', [Keycode.ALT, Keycode.LEFT_ARROW]), #Back
+        (0x004000, 'Fwd >', [Keycode.ALT, Keycode.RIGHT_ARROW]), #Forward
+        (0x400000, 'New', [Keycode.CONTROL, 'n']),    #New Window
         # 2nd row ----------
-        (0x202000, '- Size', [Keycode.CONTROL, Keycode.KEYPAD_MINUS]),
-        (0x202000, 'Size +', [Keycode.CONTROL, Keycode.KEYPAD_PLUS]),
-        (0x400000, 'Down', ' '),                     # Scroll down
-        # 3rd row ----------
-        (0x000040, 'Reload', [Keycode.CONTROL, 'r']),
-        (0x000040, 'Home', [Keycode.ALT, Keycode.HOME]),
-        (0x000040, 'Private', [Keycode.CONTROL, 'N']),
-        # 4th row ----------
-        # Adafruit in new window
-        (0x000000, 'Switch', [Keycode.CONTROL, Keycode.SHIFT, "A"]),
-
-        # Dark reader extension
-        (0x800000, 'Dark', [Keycode.ALT, Keycode.SHIFT, "D"]),
         (0x101010, 'Dev', [Keycode.CONTROL, Keycode.SHIFT, "I"]),  # Dev tools
+        (0x000040, 'Reload', [Keycode.CONTROL, 'r']), #reload
+        (0x000040, 'Private', [Keycode.CONTROL, 'N']), #new private window
+        # 3rd row ----------
+        (0x400000, 'Tab', [Keycode.CONTROL, 't']),  # New Tab               
+        (0x000000, 'Last', [Keycode.CONTROL, "9"]), #last tab
+        (0x800000, 'Close', [Keycode.CONTROL, "w"]), # Close Tab
+        # 4th row ----------
+        (0x000000, 'Prev', [Keycode.CONTROL, Keycode.SHIFT, Keycode.TAB]),
+        (0x800000, 'Min', [Keycode.ALT, Keycode.SPACE, "n"]), # Minimize window
+        (0x000000, 'Next', [Keycode.CONTROL, Keycode.TAB, ]), #next tab
         # Encoder button ---
-        (0x000000, '', [Keycode.CONTROL, 'w'])  # Close tab
+        (0x800000, 'CloseWin', [Keycode.CONTROL, Keycode.SHIFT, "w"]),  # Close window
     ]
 }
